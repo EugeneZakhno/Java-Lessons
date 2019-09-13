eaadd7c6a92b9a2dc7abb82690e1eff0342bd35a
EugeneZakhno <zohan1234567@gmail.com>
2019-09-13 21:50:01 +0400

    first commit Lesson09

---
+++ Lesson08/src/main/java/Comments.java
@@ -0,0 +1,17 @@
+public class Comments {
+
+    //      1.Comments
+
+    /*
+             2. Comments
+     */
+
+    /**    java docs
+     *
+     *
+     *       3. Comments
+     *
+     *
+     */
+
+}
---
+++ Lesson09/src/main/java/Human.java
@@ -0,0 +1,8 @@
+import com.max.Car;
+
+public class Human {
+    Car car = new Car();
+    public static void main(String[] args) {
+
+    }
+}
