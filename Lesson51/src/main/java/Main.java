46c709839a8b44ffd492d76684a274f64425e05c
EugeneZakhno <zohan1234567@gmail.com>
2019-09-15 20:56:33 +0400

    second commit Lesson51

--- Lesson51/src/main/java/Main.java
+++ Lesson51/src/main/java/Main.java
@@ -1,14 +1,62 @@
 import java.util.*;
 
+class Person /*implements Comparable<Person>*/ {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+/*    @Override
+    public int compareTo(Person o) {
+        return  this.age - o.age ;
+    }*/
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                '}';
+    }
+}
+// Comparator we use when wa have no access to class Person:
+class  ComparePerson implements Comparator <Person>{
+    @Override
+    public int compare(Person o1, Person o2) {
+        return o1.getAge() - o2.getAge();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        Set set = new TreeSet(new PersonComparator());
-        set.add(new Person(4));
-        set.add(new Person(5));
-        set.add(new Person(6));
-        set.add(new Person(8));
+
+        Set set = new TreeSet(new ComparePerson());
+
+        set.add(new Person(41));
+        set.add(new Person(70));
+        set.add(new Person(70));
+        set.add(new Person(61));
+        set.add(new Person(32));
+
         for (Object o : set){
-            System.out.println(o);
+           System.out.println(o);
+
+       /*   Начиная с Java 8, мы можем сделать так
+            Set<Person> set = new TreeSet<>(Comparator.comparingInt(Person::getAge))
+            или если для имени
+            Set<Person> set = new TreeSet<>(Comparator.comparing(Person::getName))
+            Это без написания дополнительных классов и имплементации Comparable - работает сразу.
+            И еще можно выводить коллекции так
+            set.forEach(System.out::print), чтобы не писать дополнительно цикл.
+        */
         }
     }
 }
