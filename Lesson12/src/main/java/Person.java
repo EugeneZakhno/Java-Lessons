e10b3368fd8db4778c05ec7a3402ef424870c29d
EugeneZakhno <zohan1234567@gmail.com>
2019-09-15 21:43:24 +0400

    first commit Lesson12

---
+++ Lesson12/src/main/java/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+
+    private String mr = "mr.";
+    private String name = "Max";
+
+    public String getName() {
+        return mr + name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
