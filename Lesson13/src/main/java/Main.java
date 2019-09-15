0a94cad5a87fce6842a5e7324e1f20559bc37da6
EugeneZakhno <zohan1234567@gmail.com>
2019-09-15 22:00:40 +0400

    first commit Lesson13

---
+++ Lesson13/src/main/java/Main.java
@@ -0,0 +1,19 @@
+//class is noun.
+public class Main {
+
+
+    int camelCase;
+    int $fgkh;
+    int _sdlkfg;
+    int f45744;
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+// interface is adjective
+interface Clonable{
+
+}
---
+++ Lesson13/src/main/java/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String myName;
+    private boolean blond;
+
+    //for boolean use "is"
+    public boolean isBlond(){
+        return blond;
+    }
+
+    public String getMyName() {
+        return myName;
+    }
+
+    public void setMyName(String myName) {
+        this.myName = myName;
+    }
+}
