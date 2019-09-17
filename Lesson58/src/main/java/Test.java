9d41831542be4844efb048bdadf172ef681960a4
EugeneZakhno <zohan1234567@gmail.com>
2019-09-17 22:18:14 +0200

    second commit Lesson58

---
+++ Lesson58/src/main/java/Test.java
@@ -0,0 +1,45 @@
+// Simple way for @Overriding methods
+/*
+class Animal implements  IAnimal{
+    void eat(){
+        System.out.println("The animal is eating ");
+    }
+}
+//class OtherAnimal extends Animal{
+
+*/
+/*    public void eat(){
+        System.out.println("Other animal is eating soup!!!");
+    }*//*
+
+//}
+interface IAnimal {
+   public void eat();
+}
+
+public class Test {
+    public static void main(String[] args) {
+        IAnimal iAnimal = new Animal();
+        iAnimal.eat();
+        Animal animal = new Animal();
+        animal.eat();
+
+*/
+/*
+        OtherAnimal otherAnimal = new OtherAnimal();
+        otherAnimal.eat();
+*//*
+
+
+        // @Overriding with anonymous classes
+ //       Animal animal1 = new Animal(){
+            @Override
+           public void eat(){
+              System.out.println("anonymous");
+          }
+        };
+        animal1.eat();
+
+    }
+}
+*/
