95e5862ae01702333d329a54b257b4c21225284c
EugeneZakhno <zohan1234567@gmail.com>
2019-11-24 20:59:39 +0200

    added Lesson36

---
+++ Lesson36/src/main/java/Main.java
@@ -0,0 +1,24 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Задаём ->
+                    // Edit Configurations / Program arguments
+
+        for (String s: args){
+            System.out.println(s);
+        }
+
+        /*
+        Output
+        param1
+        param2
+        param3
+        bla
+        bla
+        bla
+      */
+
+    }
+}
