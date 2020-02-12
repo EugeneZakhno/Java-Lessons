1572807689c0afb5b50870d29f38421548b4f840
EugeneZakhno <zohan1234567@gmail.com>
2020-02-12 20:32:13 +0300

    added Lesson 19 Constructors

--- Lesson18/src/main/java/Example.java
+++ Lesson18/src/main/java/Example.java
@@ -1,6 +1,17 @@
 public class Example {
+     //Урок по Java 18: Методы
+    //https://www.youtube.com/watch?v=-e83MDlKjHc
 
     public abstract static strictfp synchronized int final native metod() {
-        // passifn method(){};
+        /** passifn method(){};
+         *
+         * p - access modifier
+         * a - related to abstract class
+         * s - to class relative
+         * s - for float point, more corrective
+         * s - thread save
+         * i - return type;
+         * f - cannot override
+         */
     }
 }
