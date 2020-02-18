28dfacb339928727187d8fcda840b5e46b3504bb
EugeneZakhno <zohan1234567@gmail.com>
2020-02-18 23:26:45 +0300

    added Lesson 20 extendable

--- Lesson20/src/main/java/ArrayExample.java
+++ Lesson20/src/main/java/ArrayExample.java
@@ -3,5 +3,6 @@ public class ArrayExample {
     int [] arr = {5,4,11};
     boolean [ ] booleanArray = {true, false};
     double[] doubles = {5.4,4.0,4};
+    String [] array = new String[100];
 
 }
