6ff45295260926598759e40db916b28a515c4cdb
EugeneZakhno <zohan1234567@gmail.com>
2020-02-25 00:28:54 +0300

    added Lesson 323 refactoring

--- Lesson323/src/main/java/ImmutableLesson.java
+++ Lesson323/src/main/java/ImmutableLesson.java
@@ -1,13 +1,55 @@
+import java.util.Date;
+
 public class ImmutableLesson {
     public static void main(String[] args) {
-        String s = "fdg";
-        s= "fdg";
+        Student student = new Student(18, "Bill", new Date());
+        int i = student.getAge();
+        String s = student.getName();
+        Date d = student.getDate();
+
+        i = 1;
+        s="Make";
+        d.setTime(0);
+        System.out.println(student.getAge()+" "+ student.getName()+ " " +student.getDate());
+      /*  System.out.println();
+        Student student1 = new MyStudent(20, "fg", new Date());
+        System.out.println(student1.getDate());*/
 
     }
 }
-class Student {
+ final class Student {
     int age;
     String name;
+    Date date;
 
+    public Student(int age, String name, Date date) {
+        this.age = age;
+        this.name = name;
+        this.date = date;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDate() {
+        return (Date)date.clone();
+    }
 }
+/*class MyStudent extends Student{
+    public MyStudent(int age, String name, Date date) {
+        super(age, name, date);
+    }
+
+    @Override
+    public Date getDate() {
+        Date date = new Date();
+        date.setTime(0);
+        return date;
+    }
+
+}*/
