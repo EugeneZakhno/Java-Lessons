a040b5a1fbdf6495459dce145428510b3dc631b0
EugeneZakhno <zohan1234567@gmail.com>
2020-02-24 23:58:39 +0300

    added Lesson 323

---
+++ Lesson323/src/main/java/ImmutableLesson.java
@@ -0,0 +1,13 @@
+public class ImmutableLesson {
+    public static void main(String[] args) {
+        String s = "fdg";
+        s= "fdg";
+
+    }
+}
+class Student {
+    int age;
+    String name;
+
+
+}
