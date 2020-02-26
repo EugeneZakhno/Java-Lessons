7df9ed0d4a7aa55ee03578cd4be9b47b946f9f72
EugeneZakhno <zohan1234567@gmail.com>
2020-02-26 21:51:49 +0300

    added Lesson 23 constructors new

--- Lesson23/src/main/java/ConstructorExample.java
+++ Lesson23/src/main/java/ConstructorExample.java
@@ -1,6 +1,26 @@
-public class ConstructorExample {
 
-    public ConstructorExample(int j) {
-        super();//Даже неявно но добавляется в конструктор
+class MyParent{
+    public MyParent(int y) {
+        System.out.println(" MY parent");
     }
+
+    public MyParent() {
+        System.out.println("Defolte construcotr");
+    }
+}
+
+
+public class ConstructorExample extends MyParent {
+
+    public ConstructorExample() {
+        super(5);
+        //super();//Даже неявно но добавляется в конструктор
+        System.out.println("Constructor example");
+    }
+
+    public static void main(String[] args) {
+        new ConstructorExample();
+    }
+
+
 }
