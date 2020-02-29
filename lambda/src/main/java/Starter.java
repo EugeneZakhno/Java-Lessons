363b6a30762b985ec37b0ea57ce012693b42b900
EugeneZakhno <zohan1234567@gmail.com>
2020-02-29 21:11:21 +0300

    added lambda package new again

--- lambda/src/main/java/Starter.java
+++ lambda/src/main/java/Starter.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Starter {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
         for (int i = 10; i > 0; i--) {
             list.add(String.format("%2d", i));
         }
