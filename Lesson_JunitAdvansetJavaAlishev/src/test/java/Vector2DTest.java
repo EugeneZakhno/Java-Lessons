c8d3aed74061ff38590ddeca07c2dc9fdfc6295a
EugeneZakhno <zohan1234567@gmail.com>
2020-03-06 23:31:00 +0300

    added LessonAdvasetJavaAlishev

--- Lesson_JunitAdvansetJavaAlishev/src/test/java/Vector2DTest.java
+++ Lesson_JunitAdvansetJavaAlishev/src/test/java/Vector2DTest.java
@@ -4,8 +4,6 @@ class Vector2DTest {
 
     @org.junit.jupiter.api.Test
     public void newVectorShouldHaveZiroLength() {
-            Vector2D v1 = new Vector2D();
-            assertEquals(0, v1.length(), 0 );
 
     }
 }
